2a8e23f90754129fc871ab119e165965e4700f2f
Oya <marciooya@gmail.com>
2022-05-19 16:27:10 -0300

    base

---
+++ php/bookingForm.php
@@ -0,0 +1,26 @@
+<?
+$date = $_REQUEST["date"];
+$name  = $_REQUEST["name"];
+$email = $_REQUEST["email"];
+$phone  = $_REQUEST["phone"];
+$msg   = $_REQUEST["msg"];
+$to    = "you@yourdomain.com"; // ENTER YOUR EMAIL ADDRESS
+if (isset($email) && isset($name) && isset($msg)) {
+    $email_subject = "$name sent you a message via YOUR SITE NAME"; // ENTER YOUR EMAIL SUBJECT
+		$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+$headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
+$msg     = "Date: $date <br/> Name: $name <br/> Email: $email <br/> Phone Number: $phone <br/> Message: $msg";
+   $mail =  mail($to, $email_subject, $msg, $headers);
+  if($mail)
+	{
+		echo 'success';
+	}
+
+else
+	{
+		echo 'failed';
+	}
+}
+
+?>
